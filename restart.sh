20f48db2c8bbd11ac874214aef2c2a1368610dd4
Mejdi Mejdoub <mejdi.mejdoub@ledger.fr>
2022-06-20 15:57:45 +0200

    First commit

---
+++ restart.sh
@@ -0,0 +1,10 @@
+docker rm -f my-dashboard
+docker run -d \
+  -p 8081:80 \
+  -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/conf.yml:/app/public/conf.yml \
+  -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/custom/dev.yml:/app/public/dev.yml \
+  -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/custom/common.yml:/app/public/common.yml \
+  -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/custom/crypto.yml:/app/public/crypto.yml \
+  --name my-dashboard \
+  --restart=always \
+  lissy93/dashy:latest
