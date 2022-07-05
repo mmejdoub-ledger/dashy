e66c4ef4b0a9b41c974073703763a71554b83d30
Mejdi Mejdoub <mejdi.mejdoub@ledger.fr>
2022-07-05 18:21:20 +0200

    add chart for sbx

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@ docker rm -f my-dashboard
 docker run -d \
   -p 8081:80 \
   -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/conf.yml:/app/public/conf.yml \
-  -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/custom/dev.yml:/app/public/dev.yml \
+  -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/custom/dev.yml:/app/public/pages/dev.yml \
   -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/custom/common.yml:/app/public/common.yml \
   -v /Users/mejdi.mejdoub/work/pg/tmp/dashy/custom/crypto.yml:/app/public/crypto.yml \
   --name my-dashboard \
